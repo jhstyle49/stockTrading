2954240b03ad6213ead76fb7275200303e605250
jinhoon.kim <jhstyle49@gmail.com>
2024-12-27 06:51:36 +0900

    gradio env reset & S&P500rateoreturn code add

--- gradioblocks.py
+++ gradioblocks.py
@@ -1,4 +1,24 @@
+import yfinance as yf 
+import pandas as pd
+import numpy as np
+
 import gradio as gr
+
+ticker_usd = ['^GSPC', '^SP500TR']
+result=yf.download(ticker_usd, start='1900-01-01')
+result.columns = ['_'.join(c) for c in result.columns]
+result = result.reset_index()
+result['Date'] = result['Date'].dt.strftime('%Y-%m-%d')
+result['Date'] = pd.to_datetime(result['Date'])
+
+remain_cols = []
+for t in ticker_usd:
+    remain_cols.append(f"Close_{t}")
+    
+result = result[['Date']+remain_cols]
+result = result.dropna()
+result_mon = result.copy()
+result_year = result.copy()
 
 def greet(name, intensity):
     return "Hello, " + name + "!" * int(intensity)
@@ -13,3 +33,4 @@ demo = gr.Interface(
 )
 
 demo.launch()
+
